eb0ff348c25d611985182afda89c841b6d49658a
xcsalo00 <jasminacsalova@gmail.com>
2022-11-20 18:41:55 +0100

    Laravel tutorial done

---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+//TODO:
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// all listings
+Route::get('/', [ListingController::class, 'index']);
+
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing  
+
+// show create form - listing
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+// show store listing
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
+
+//show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+
+//edit submit to update, update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
+
+//delete listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
+
+//manage listings
+Route::get('/listings/manage', [ListingController::class, 'manage'])->middleware('auth');
+
+// single listing -- MUST BE THE LAST ONE!!!!!
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+//show register/create form
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+
+//create a new user - storing him
+Route::post('/users', [UserController::class, 'store']);
+
+//log out of user
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+//SHOW LOGIN FROM
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+//olg in user
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
+
